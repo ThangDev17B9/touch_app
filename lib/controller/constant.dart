d534995993f02216a718ad4c934cfd29f1aff5c8
Thangnq <nq.thangdev@gmail.com>
2025-02-17 10:59:47 +0700

    Delete lib/controller/constant.dart
    fix

--- lib/controller/constant.dart
+++
@@ -1,8 +0,0 @@
-class Constant {
-  //api
-  static const String getProject = 'https://cms.bolttech.space/graphql';
-  static const String postFile = 'https://cms.bolttech.space/files';
-  static const String postInforFile = 'https://cms.bolttech.space/graphql';
-  //token
-  static const String token = 'KjzcGYUfUJQdLca7SEx2hpZkXe5STOwj';
-}
